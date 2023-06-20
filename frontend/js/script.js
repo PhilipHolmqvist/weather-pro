d238684989186b7d5aef46ee6d6fa1b44173e747
Philip Holmqvist <philip_holmqvist@hotmail.com>
2023-06-20 12:10:11 +0200

    dynmaisk html och setup för api anrop

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,21 +1,63 @@
+
+  const main = document.querySelector('main')
+
+  let params = (new URL(document.location)).searchParams;
+  weather = getWeather(params.get("q"));
+
+  for(let i = 0; i < weather.length; i++){
+    console.log(weather['klart'])
+  }
+
+
+  let htmlString = "";
+  let cityName = "Helsingborg";
+  let weatherType = "Cloudy";
+  let windSpeed = "10";
+  let rainChance = "0";
+  let temperature = "23";
+  let forecasterName = "klart"
+
+
+  //Iterera genom JSON response från API.
+  for(let i = 0; i < 4; i++){
+    htmlString += `
+    <div class="card">
+      <img src="../images/${forecasterName}-logo.png" alt="${forecasterName}" width="80" height="50">
+      <h2>${cityName}</h2>
+      <h3>${weatherType}<span>Vind ${windSpeed}/s<span class="dot">•</span>Regn ${rainChance}</span></h3>
+      <h1>${temperature}°</h1>
+      <div class="sky">
+        <div class="sun"></div>
+        <div class="cloud">
+          <div class="circle-small"></div>
+          <div class="circle-tall"></div>
+          <div class="circle-medium"></div>
+        </div>
+      </div>
+    </div>`;
+  }
+
+main.innerHTML = htmlString;
+
+
 function search() {
     var searchText = document.getElementById("search").value;
     if (searchText) {
       // Redirect to search results page with the search text.
-      window.location.href = "search.html?query=" + encodeURIComponent(searchText);
+      window.location.href = "search.html?q=" + encodeURIComponent(searchText);
       console.log('Search text: ' + searchText)
       getWeather(searchText);
     }
   }
   
-  function quickSearch(option) {
+function quickSearch(option) {
     // Redirect to quick search results page with the selected option
-    window.location.href = "search.html?option=" + encodeURIComponent(option);
+    window.location.href = "search.html?q=" + encodeURIComponent(option);
     console.log('Search text: ' + searchText)
     getWeather(searchText);
   }
 
-  async function getWeather(city){
+async function getWeather(city){
     var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
     
     if (city == '' || !format.test(city)){
@@ -35,7 +77,6 @@ function search() {
   
       if (response.ok) {
         const weather = await response.json();
-        return weather;
       } else {
         throw new Error('Failed to get weather for city: ' + city);
       }
@@ -44,5 +85,15 @@ function search() {
       return [];
     }
   }
+
+//Search with press of enter key.
+var input = document.getElementById("search");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("search-button").click();
+    }
+  });
+
 
   
